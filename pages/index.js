e7a14b26211c68e848800af3e229c75a40b45f19
jayseaCODE <jayseaLEARN@hotmail.com>
2019-09-08 16:53:11 +1200

    feat: pages that support dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,84 @@
 import Link from 'next/link';
 //import withLayout from '../components/Layout';
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
+class Index extends React.Component
+{
+  constructor(props)
+  {
+    super(props)
+    var ex = props.shows;
+  }
+  render() {
+    return (
+      <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+          {this.props.shows.map(show => (
+            <li key={show.id}>
+              <Link href="/p/[id]" as={`/p/${show.id}`}>
+                <a>{show.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    );
+  }
+}
+/*
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+*/
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
 
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
+
+/*
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+    </Layout>
+  );
+}
+*/
+/*
 const PostLink = props => (
   <li>
     <Link href={`/post?title=${props.title}`}>
@@ -32,6 +108,7 @@ export default function Blog() {
     </Layout>
   );
 }
+*/
 /*
 const Page = () => <p>Welcome to my Sandbox</p>
   
